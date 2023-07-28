791b269778baf98d68e4bac72f66e33b67251e22
Luismonsalver <126512492+Luismonsalver@users.noreply.github.com>
2023-07-28 15:50:43 -0400

    Diseño y funcionalidades

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,5 @@
+let questionCounter = 0; // Variable para contar el número de preguntas agregadas
+
 // Función para añadir una nueva pregunta al contenedor
 function addQuestion() {
   const question = document.getElementById("question").value;
@@ -9,8 +11,8 @@ function addQuestion() {
   }
 
   let questionElement = document.createElement("div");
-  questionElement.classList.add("question")
-  questionElement.innerHTML = `<h2>Pregunta:</h2><p>${question}</p>`;
+  questionElement.classList.add("question");
+  questionElement.innerHTML = `<h3>Pregunta:</h3><p>${question}</p>`;
 
   if (questionType === "text") {
     questionElement.innerHTML += '<input type="text" placeholder="Respuesta">';
@@ -31,13 +33,27 @@ function addQuestion() {
   }
 
   document.getElementById("questionsContainer").appendChild(questionElement);
+  questionCounter++;
+
+  // Mostrar el botón de enviar si hay al menos una pregunta
+  if (questionCounter === 1) {
+    const sendButton = document.createElement("button");
+    sendButton.textContent = "Enviar";
+    sendButton.onclick = submitForm;
+    document.getElementById("questionsContainer").appendChild(sendButton);
+  }
+}
+
+// Función para enviar el formulario a la base de datos (implementación ficticia)
+function submitForm() {
+  alert("¡Formulario enviado!");
 }
 
 // Mostrar u ocultar el campo de opciones dependiendo del tipo de pregunta seleccionada
 document.getElementById("questionType").addEventListener("change", function () {
   const optionsContainer = document.getElementById("optionsContainer");
-  if (this.value === "options"||"select") {
-    optionsContainer.style.display = "block";
+  if (this.value === "options"||this.value === "select") {
+    optionsContainer.style.display = "flex";
   } else {
     optionsContainer.style.display = "none";
   }
